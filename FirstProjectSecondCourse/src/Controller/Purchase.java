f42991e0e62e93f08be423ab7276755e32410a88
stefaniehd <30412217+stefaniehd@users.noreply.github.com>
2017-10-29 15:22:17 -0600

    Controller clases are already added.

---
+++ FirstProjectSecondCourse/src/Controller/Purchase.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author pc
+ */
+public class Purchase {
+    private String id;
+    private String purchaseNum;
+    private String user;
+    private String disc;
+    private int cant;
+
+    public Purchase() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPurchaseNum() {
+        return purchaseNum;
+    }
+
+    public void setPurchaseNum(String purchaseNum) {
+        this.purchaseNum = purchaseNum;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getDisc() {
+        return disc;
+    }
+
+    public void setDisc(String disc) {
+        this.disc = disc;
+    }
+
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        this.cant = cant;
+    }
+}
