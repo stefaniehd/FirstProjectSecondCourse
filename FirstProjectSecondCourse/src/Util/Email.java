a2918b49656495211d1f45d32a8401a8a7e0c565
stefaniehd <30412217+stefaniehd@users.noreply.github.com>
2017-10-29 15:54:35 -0600

    Model folder is already modificated.

---
+++ FirstProjectSecondCourse/src/Util/Email.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+/**
+ *
+ * @author pc
+ */
+public class Email {
+    
+    private String subject;
+    private String message;
+    private String email;
+
+    public Email() {
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
